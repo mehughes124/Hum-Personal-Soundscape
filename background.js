2b1434130bd8d6770399805f075ed277e4ad6356
mehughes124 <mehughes124@gmail.com>
2020-08-31 14:43:36 -0400

    Add files via upload

---
+++ background.js
@@ -0,0 +1,376 @@
+
+
+chrome.runtime.onInstalled.addListener(function() {
+  // set all needed values for page build
+  chrome.storage.sync.set({player_volume: '0.3', muted: 'inactive'}, function() {
+    console.log("player volume has been set");
+  });
+  
+  // set all individual howl volume levels
+  chrome.storage.sync.set({coffeeShop_volume: '0.5', waves_volume: '0.5', leaves_volume: '0.5', rain_volume: '0.5', fire_volume: '0.5', river_volume: '0.5', night_volume: '0.5', fan_volume: '0.5'}, function() {
+    console.log("player volume has been set");
+  });
+  
+  // set all individual howl states as inactive
+  chrome.storage.sync.set({coffeeShopState: 'inactive', wavesState: 'inactive', leavesState: 'inactive', rainState: 'inactive', fireState: 'inactive', riverState: 'inactive', nightState: 'inactive', fanState: 'inactive'}, function() {
+    console.log("button states have been set");
+  });
+  
+  // make sure initial player volume value is passed to Howler
+  chrome.storage.sync.get(['player_volume'], function(result) {
+    Howler.volume(result.player_volume);
+  });
+});
+
+// declare sounds as empty objects so we can check if they are created as howls yet later without throwing an error
+var coffeeShopSound = {}, 
+wavesSound = {}, 
+leavesSound = {}, 
+rainSound = {}, 
+fireSound = {}, 
+riverSound = {}, 
+nightSound = {}, 
+fanSound = {};
+
+// use a boolean for backend muted state and make sure it doesn't get out of sync with storage
+var mutedBool = false;
+chrome.storage.sync.set({muted: 'inactive'});
+
+// this is an array we'll push the created, actively playing Howls to.
+// Not used for anything right now, but I have the removeItem function for a reason that didn't pan out and I might as well keep it in case it's useful.
+var currentlyPlaying = [];
+
+// big gnarly block for listening for the popup.js calls.
+// return true at the end is to make it asynchronous so popup.js knows to wait to call buildPage()
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.action === 'pushButton') {
+      switch (request.value) {
+        case 'coffeeShopSound':
+            if (toggleHowl(coffeeShopSound) === 'active'){
+              chrome.storage.sync.set({coffeeShopState: 'active'});
+            }
+            else{
+              chrome.storage.sync.set({coffeeShopState: 'inactive'});
+            }
+          break;
+        case 'wavesSound':
+          if (toggleHowl(wavesSound) === 'active'){
+              chrome.storage.sync.set({wavesState: 'active'});
+            }
+            else{
+              chrome.storage.sync.set({wavesState: 'inactive'});
+            }
+          break;
+        case 'leavesSound':
+          if (toggleHowl(leavesSound) === 'active'){
+              chrome.storage.sync.set({leavesState: 'active'});
+            }
+            else{
+              chrome.storage.sync.set({leavesState: 'inactive'});
+            }
+          break;
+        case 'rainSound':
+          if (toggleHowl(rainSound) === 'active'){
+              chrome.storage.sync.set({rainState: 'active'});
+            }
+            else{
+              chrome.storage.sync.set({rainState: 'inactive'});
+            }
+          break;
+        case 'fireSound':
+          if (toggleHowl(fireSound) === 'active'){
+              chrome.storage.sync.set({fireState: 'active'});
+            }
+            else{
+              chrome.storage.sync.set({fireState: 'inactive'});
+            }
+          break;
+        case 'riverSound':
+          if (toggleHowl(riverSound) === 'active'){
+              chrome.storage.sync.set({riverState: 'active'});
+            }
+            else{
+              chrome.storage.sync.set({riverState: 'inactive'});
+            }
+          break;
+        case 'nightSound':
+          if (toggleHowl(nightSound) === 'active'){
+              chrome.storage.sync.set({nightState: 'active'});
+            }
+            else{
+              chrome.storage.sync.set({nightState: 'inactive'});
+            }
+          break;
+        case 'fanSound':
+          if (toggleHowl(fanSound) === 'active'){
+              chrome.storage.sync.set({fanState: 'active'});
+            }
+            else{
+              chrome.storage.sync.set({fanState: 'inactive'});
+            }
+          break;
+      }
+    }
+    else if(request.action ==='toggle_mute'){
+      console.log("background received mute button click");
+      // check mutedBool. If it is true, that means we are currently muted, and need to toggle it
+      if (mutedBool){
+        chrome.storage.sync.get(['player_volume'], function(result) {
+            Howler.volume(result.player_volume);
+          });
+        chrome.storage.sync.set({muted: 'inactive'});
+        mutedBool = false;
+      }
+      //else, muted is currently true as of the message, so we need to toggle it
+      else{
+        Howler.volume('0');
+        chrome.storage.sync.set({muted: 'active'});
+        mutedBool = true;
+      }
+    }
+    else if(request.action ==='volume'){
+      chrome.storage.sync.get(['player_volume'], function(result) {
+        Howler.volume(result.player_volume);
+      });
+      chrome.storage.sync.set({muted: 'inactive'});
+      mutedBool = false;
+    }
+    else if (request.action === 'coffeeShop_volume'){
+      chrome.storage.sync.get(['coffeeShop_volume'], function(result) {
+        if (checkIfHowlHasBeenCreated(coffeeShopSound)){
+          coffeeShopSound.volume(result.coffeeShop_volume);
+        }
+      });
+    }
+    else if (request.action === 'waves_volume'){
+      chrome.storage.sync.get(['waves_volume'], function(result) {
+        // if "playing" is in howl, set the volume
+        if (checkIfHowlHasBeenCreated(wavesSound)){
+          wavesSound.volume(result.waves_volume);
+        }
+      });
+    }
+    else if (request.action === 'leaves_volume'){
+      chrome.storage.sync.get(['leaves_volume'], function(result) {
+        if (checkIfHowlHasBeenCreated(leavesSound)){
+          leavesSound.volume(result.leaves_volume);
+        }
+      });
+    }
+    else if (request.action === 'rain_volume'){
+      chrome.storage.sync.get(['rain_volume'], function(result) {
+        if (checkIfHowlHasBeenCreated(rainSound)){
+          rainSound.volume(result.rain_volume);
+        }
+      });
+    }
+    else if (request.action === 'fire_volume'){
+      chrome.storage.sync.get(['fire_volume'], function(result) {
+        if (checkIfHowlHasBeenCreated(fireSound)){
+          fireSound.volume(result.fire_volume);
+        }
+      });
+    }
+    else if (request.action === 'river_volume'){
+      chrome.storage.sync.get(['river_volume'], function(result) {
+        if (checkIfHowlHasBeenCreated(riverSound)){
+          riverSound.volume(result.river_volume);
+        }
+      });
+    }
+    else if (request.action === 'night_volume'){
+      chrome.storage.sync.get(['night_volume'], function(result) {
+        if (checkIfHowlHasBeenCreated(nightSound)){
+          nightSound.volume(result.night_volume);   
+        }
+      });
+    }
+    else if (request.action === 'fan_volume'){
+      chrome.storage.sync.get(['fan_volume'], function(result) {
+        if (checkIfHowlHasBeenCreated(fanSound)){
+          fanSound.volume(result.fan_volume); 
+        }
+      });
+    }
+    else if(request.action ==='resume'){
+      console.log("Play button was pushed and message received in background");
+      currentlyPlaying.forEach(resumeSounds);
+    }
+  
+  sendResponse({update: "update"});
+  return true;
+});
+
+// a little StackOverflow copy-paste to splice out an array element without creating a new array
+function removeItem(array, value) {
+  var index = array.indexOf(value);
+  if (index > -1) {
+    array.splice(index, 1);
+  }
+  return array;
+}
+
+// instantiate new Howl for each sound.
+// Would be nice to figure out how to not have to repeat all of the properties that are the same for each Howl.
+// the "is_playing" property isn't a Howler howl property, and I'm not using it for anything, so I should probably just take it out. TBD.
+function createHowl(sound){
+  switch(sound){
+    case coffeeShopSound:
+      console.log("switch case in howl generator worked");
+      coffeeShopSound = new Howl({
+        src: ['sounds/coffeeShop.mp3'],
+        loop: true,
+        html5: true,
+        volume: 0.5,
+        name: 'coffeeShopSound',
+        is_playing: true
+      });
+      chrome.storage.sync.get(['coffeeShop_volume'], function(result) {
+        coffeeShopSound.volume(result.coffeeShop_volume);
+      });
+      coffeeShopSound.play();
+      chrome.storage.sync.set({coffeeShopState: 'active'});
+      break;
+    case wavesSound:
+      wavesSound = new Howl({
+        src: ['sounds/waves.mp3'],
+        loop: true,
+        html5: true,
+        volume: 0.5,
+        name: 'wavesSound',
+        is_playing: true
+      });
+      chrome.storage.sync.get(['waves_volume'], function(result) {
+        wavesSound.volume(result.waves_volume);
+      });
+      wavesSound.play();
+      chrome.storage.sync.set({wavesState: 'active'});
+      break;
+    case leavesSound:
+      leavesSound = new Howl({
+        src: ['sounds/leaves.mp3'],
+        loop: true,
+        html5: true,
+        volume: 0.5,
+        name: 'leavesSound',
+        is_playing: true
+      });
+      chrome.storage.sync.get(['leaves_volume'], function(result) {
+        leavesSound.volume(result.leaves_volume);
+      });
+      leavesSound.play();
+      chrome.storage.sync.set({leavesState: 'active'});
+      break;
+    case rainSound:
+      rainSound = new Howl({
+        src: ['sounds/rain.mp3'],
+        loop: true,
+        html5: true,
+        volume: 0.5,
+        name: 'rainSound',
+        is_playing: true
+      });
+      chrome.storage.sync.get(['rain_volume'], function(result) {
+        rainSound.volume(result.rain_volume);
+      });
+      rainSound.play();
+      chrome.storage.sync.set({rainState: 'active'});
+      break;
+    case fireSound:
+      fireSound = new Howl({
+        src: ['sounds/fire.mp3'],
+        loop: true,
+        html5: true,
+        volume: 0.5,
+        name: 'fireSound',
+        is_playing: true
+      });
+      chrome.storage.sync.get(['fire_volume'], function(result) {
+        fireSound.volume(result.fire_volume);
+      });
+      fireSound.play();
+      chrome.storage.sync.set({fireState: 'active'});
+      break;
+    case riverSound:
+      riverSound = new Howl({
+        src: ['sounds/river.mp3'],
+        loop: true,
+        html5: true,
+        volume: 0.5,
+        name: 'riverSound',
+        is_playing: true
+      });
+      chrome.storage.sync.get(['river_volume'], function(result) {
+        riverSound.volume(result.river_volume);
+      });
+      riverSound.play();
+      chrome.storage.sync.set({riverState: 'active'});
+      break;
+    case nightSound:
+      nightSound = new Howl({
+        src: ['sounds/night.mp3'],
+        loop: true,
+        html5: true,
+        volume: 0.5,
+        name: 'nightSound',
+        is_playing: true
+      });
+      chrome.storage.sync.get(['night_volume'], function(result) {
+        nightSound.volume(result.night_volume);
+      });
+      nightSound.play();
+      chrome.storage.sync.set({nightState: 'active'});
+      break;
+    case fanSound:
+      fanSound = new Howl({
+        src: ['sounds/fan.mp3'],
+        loop: true,
+        html5: true,
+        volume: 0.5,
+        name: 'fanSound',
+        is_playing: true
+      });
+      chrome.storage.sync.get(['fan_volume'], function(result) {
+        fanSound.volume(result.fan_volume);
+      });
+      fanSound.play();
+      chrome.storage.sync.set({fanState: 'active'});
+      break;
+  }
+}
+
+function checkIfHowlHasBeenCreated(sound){
+  if (!('playing' in sound)){
+    return false;
+  }
+  else{
+    return true;
+  }
+}
+
+function toggleHowl(sound){
+  // check to see if the sound object has been turned into a Howl yet
+  if (!('playing' in sound)){
+    createHowl(sound);
+    // part of createHowl is to call .play on the new howl and set its corresponding state in storage, so return active
+    return "active";
+  }
+  else{
+    if (sound.playing()){
+      console.log(sound + "the toggle function pause fired");
+      sound.pause();
+      sound.is_playing = false;
+      removeItem(currentlyPlaying, sound);
+      return "inactive";
+    }
+    else if (!(sound.playing())){
+      sound.play();
+      console.log(sound + "the toggle function play fired");
+      sound.is_playing = true;
+      currentlyPlaying.push(sound);
+      return "active";
+    }
+  }
+}
+
---
+++ popup.js
@@ -0,0 +1,259 @@
+
+var coffeeShopButton = document.getElementById('coffeeShopButton');
+var wavesButton = document.getElementById('wavesButton');
+var leavesButton = document.getElementById('leavesButton');
+var rainButton = document.getElementById('rainButton');
+var fireButton = document.getElementById('fireButton');
+var riverButton = document.getElementById('riverButton');
+var nightButton = document.getElementById('nightButton');
+var fanButton = document.getElementById('fanButton');
+
+var muteButton = document.getElementById('muteButton');
+
+var masterVolumeSlider = document.getElementById('masterVolumeSlider');
+
+var coffeeShopVolumeSlider = document.getElementById('coffeeShopVolumeSlider');
+var wavesVolumeSlider = document.getElementById('wavesVolumeSlider');
+var leavesVolumeSlider = document.getElementById('leavesVolumeSlider');
+var rainVolumeSlider = document.getElementById('rainVolumeSlider');
+var fireVolumeSlider = document.getElementById('fireVolumeSlider');
+var riverVolumeSlider = document.getElementById('riverVolumeSlider');
+var nightVolumeSlider = document.getElementById('nightVolumeSlider');
+var fanVolumeSlider = document.getElementById('fanVolumeSlider');
+
+
+// this is called on page load, and then on any successful response from .sendMessage on events
+function buildPage(){
+  chrome.storage.sync.get(['player_volume',
+    'coffeeShop_volume', 
+    'waves_volume',
+    'leaves_volume',
+    'rain_volume',
+    'fire_volume',
+    'river_volume',
+    'night_volume',
+    'fan_volume',
+    'muted'], function(key) {
+    masterVolumeSlider.value = key.player_volume;
+    coffeeShopVolumeSlider.value = key.coffeeShop_volume;
+    wavesVolumeSlider.value = key.waves_volume;
+    leavesVolumeSlider.value = key.leaves_volume;
+    rainVolumeSlider.value = key.rain_volume;
+    fireVolumeSlider.value = key.fire_volume;
+    riverVolumeSlider.value = key.river_volume;
+    nightVolumeSlider.value = key.night_volume;
+    fanVolumeSlider.value = key.fan_volume;
+  });
+  
+  // set sound buttons to active if their corresponding audio is playing
+  chrome.storage.sync.get(['coffeeShopState',
+    'wavesState',
+    'leavesState',
+    'rainState',
+    'fireState',
+    'riverState',
+    'nightState',
+    'fanState',
+    'muted'], function(key) {
+    if (key.coffeeShopState === 'active'){
+      coffeeShopButton.classList.add("active-button");
+    }
+    else{
+      coffeeShopButton.classList.remove("active-button");
+    }
+    if (key.wavesState === 'active'){
+      wavesButton.classList.add("active-button");
+    }
+    else{
+      wavesButton.classList.remove("active-button");
+    }
+    if (key.leavesState === 'active'){
+      console.log("leaves state = active");
+      leavesButton.classList.add("active-button");
+    }
+    else{
+      console.log("leaves state = inactive");
+      leavesButton.classList.remove("active-button");
+    }
+    if (key.rainState === 'active'){
+      rainButton.classList.add("active-button");
+    }
+    else{
+      rainButton.classList.remove("active-button");
+    }
+    if (key.fireState === 'active'){
+      fireButton.classList.add("active-button");
+    }
+    else{
+      fireButton.classList.remove("active-button");
+    }
+    if (key.riverState === 'active'){
+      riverButton.classList.add("active-button");
+    }
+    else{
+      riverButton.classList.remove("active-button");
+    }
+    if (key.nightState === 'active'){
+      nightButton.classList.add("active-button");
+    }
+    else{
+      nightButton.classList.remove("active-button");
+    }
+    if (key.fanState === 'active'){
+      fanButton.classList.add("active-button");
+    }
+    else{
+      fanButton.classList.remove("active-button");
+    }
+
+    if(key.muted === 'active'){
+      muteButton.classList.add("active-button");
+      muteButton.style.backgroundImage = "url('images/volume-mute.png')";
+      muteButton.style.backgroundRepeat = "no-repeat";
+      console.log("muted === 'active'");
+    }
+    else if (key.muted === 'inactive'){
+      muteButton.classList.remove("active-button");
+      muteButton.style.backgroundImage = "url('images/Speaker_Icon.png')";
+      muteButton.style.backgroundRepeat = "no-repeat";
+      console.log("muted === 'inactive'");
+    }
+  });
+  
+  console.log("buildPage() was called");
+}
+
+buildPage();
+
+muteButton.addEventListener("click", function(){
+  chrome.runtime.sendMessage({action: 'toggle_mute'},function(response) {
+    if(response.update === "update"){buildPage();}
+  });
+});
+
+masterVolumeSlider.onchange = function() {
+    volumeLevel = this.value.toString();
+    // localStorage.setItem('player_volume', volumeLevel);
+    chrome.storage.sync.set({'player_volume': volumeLevel});
+    // , function() {
+    //     console.log('volume is set to ' + volumeLevel);
+    //   });
+    chrome.runtime.sendMessage({action: 'volume', value: volumeLevel},function(response) {
+      if(response.update === "update"){buildPage();}
+    });
+};
+
+// need to learn how to abstract this to a group of pre-set elements instead of having to repeat the code for every one.
+coffeeShopButton.addEventListener("click", function(){
+  chrome.runtime.sendMessage({action: 'pushButton', value: 'coffeeShopSound'}, function(response) {
+    if(response.update === "update"){buildPage();}
+  });
+});
+
+wavesButton.addEventListener("click", function(){
+  chrome.runtime.sendMessage({action: 'pushButton', value: 'wavesSound'},function(response) {
+    if(response.update === "update"){buildPage();}
+  });
+});
+
+leavesButton.addEventListener("click", function(){
+  chrome.runtime.sendMessage({action: 'pushButton', value: 'leavesSound'},function(response) {
+    if(response.update === "update"){buildPage();}
+  });
+});
+
+rainButton.addEventListener("click", function(){
+  chrome.runtime.sendMessage({action: 'pushButton', value: 'rainSound'},function(response) {
+    if(response.update === "update"){buildPage();}
+  });
+});
+
+fireButton.addEventListener("click", function(){
+  chrome.runtime.sendMessage({action: 'pushButton', value: 'fireSound'},function(response) {
+    if(response.update === "update"){buildPage();}
+  });
+});
+
+riverButton.addEventListener("click", function(){
+  chrome.runtime.sendMessage({action: 'pushButton', value: 'riverSound'},function(response) {
+    if(response.update === "update"){buildPage();}
+  });
+});
+
+nightButton.addEventListener("click", function(){
+  chrome.runtime.sendMessage({action: 'pushButton', value: 'nightSound'},function(response) {
+    if(response.update === "update"){buildPage();}
+  });
+});
+
+fanButton.addEventListener("click", function(){
+  chrome.runtime.sendMessage({action: 'pushButton', value: 'fanSound'},function(response) {
+    if(response.update === "update"){buildPage();}
+  });
+});
+
+
+// need to learn how to abstract this to a group of pre-set elements instead of having to repeat the code for every one.
+coffeeShopVolumeSlider.onchange = function() {
+    volumeLevel = this.value.toString();
+    // localStorage.setItem('player_volume', volumeLevel);
+    chrome.storage.sync.set({'coffeeShop_volume': volumeLevel}, function() {
+      });
+    chrome.runtime.sendMessage({action: 'coffeeShop_volume', value: volumeLevel});
+};
+
+wavesVolumeSlider.onchange = function() {
+    volumeLevel = this.value.toString();
+    // localStorage.setItem('player_volume', volumeLevel);
+    chrome.storage.sync.set({'waves_volume': volumeLevel}, function() {
+      });
+    chrome.runtime.sendMessage({action: 'waves_volume', value: volumeLevel});
+};
+
+leavesVolumeSlider.onchange = function() {
+  volumeLevel = this.value.toString();
+  // localStorage.setItem('player_volume', volumeLevel);
+  chrome.storage.sync.set({'leaves_volume': volumeLevel}, function() {
+    });
+  chrome.runtime.sendMessage({action: 'leaves_volume', value: volumeLevel});
+};
+
+rainVolumeSlider.onchange = function() {
+  volumeLevel = this.value.toString();
+  // localStorage.setItem('player_volume', volumeLevel);
+  chrome.storage.sync.set({'rain_volume': volumeLevel}, function() {
+    });
+  chrome.runtime.sendMessage({action: 'rain_volume', value: volumeLevel});
+};
+
+fireVolumeSlider.onchange = function() {
+  volumeLevel = this.value.toString();
+  // localStorage.setItem('player_volume', volumeLevel);
+  chrome.storage.sync.set({'fire_volume': volumeLevel}, function() {
+    });
+  chrome.runtime.sendMessage({action: 'fire_volume', value: volumeLevel});
+};
+
+riverVolumeSlider.onchange = function() {
+  volumeLevel = this.value.toString();
+  // localStorage.setItem('player_volume', volumeLevel);
+  chrome.storage.sync.set({'river_volume': volumeLevel}, function() {
+    });
+  chrome.runtime.sendMessage({action: 'river_volume', value: volumeLevel});
+};
+
+nightVolumeSlider.onchange = function() {
+  volumeLevel = this.value.toString();
+  // localStorage.setItem('player_volume', volumeLevel);
+  chrome.storage.sync.set({'night_volume': volumeLevel}, function() {
+    });
+  chrome.runtime.sendMessage({action: 'night_volume', value: volumeLevel});
+};
+
+fanVolumeSlider.onchange = function() {
+  volumeLevel = this.value.toString();
+  // localStorage.setItem('player_volume', volumeLevel);
+  chrome.storage.sync.set({'fan_volume': volumeLevel}, function() {
+    });
+  chrome.runtime.sendMessage({action: 'fan_volume', value: volumeLevel});
+};
